8f469376b4301346eb8d7034dbf8cf54891ef01e
Lucifer3101 <namtranhtc826@gmail.com>
2021-03-25 10:39:02 +0700

    app user

--- mysite/settings.py
+++ mysite/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'user.apps.UserConfig',
     'shop.apps.ShopConfig',
     'home.apps.HomeConfig',
     'django.contrib.admin',
--- user/urls.py
+++ user/urls.py
@@ -2,5 +2,5 @@ from django.contrib import admin
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('shop/', views.shop, name="shopPage"),
+    path('user/', views.user, name="userPage"),
 ]
