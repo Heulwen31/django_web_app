b3d3e757369721d7514af2120f5ec48fb84e4a7f
Viet Tran <tviet764@gmail.com>
2021-03-26 21:21:46 +0700

    add contact

--- home/urls.py
+++ home/urls.py
@@ -6,4 +6,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name="homePage"),
+    path('contact/', views.contact, name="contact"),
+    path('home/', views.home, name="home"),
 ]
--- home/views.py
+++ home/views.py
@@ -4,9 +4,9 @@ from django.http import HttpResponse
 
 
 def home(request):
-    context ={ 
-        "data":"Gfg is the best", 
-        "list":[1, 2, 3, 4, 5, 6, 7, 8, 9, 10] 
-    } 
     # return response with template and context 
-    return render(request, "home/index.html", context) 
+    return render(request, "home/index.html") 
+
+
+def contact(request):
+    return render(request, "home/contact.html")
