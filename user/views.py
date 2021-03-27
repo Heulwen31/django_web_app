06db65f86aaa15af3b7b5835f10da6415863602a
Lucifer3101 <namtranhtc826@gmail.com>
2021-03-27 22:07:13 +0700

    login and register user

--- user/views.py
+++ user/views.py
@@ -1,13 +1,53 @@
-from django.shortcuts import render
+
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
-# Create your views here.
+from django.forms import inlineformset_factory
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from .forms import CreateUserForm
 
-
-def user(request):
-    return HttpResponse("<h1> hello <h1/>")
 
 def login(request):
-    return render(request, "user/login.html")
+    if request.user.is_authenticated:
+        return redirect('home')
+    else:
+        if request.method == 'POST':
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+
+            user = authenticate(request, username=username, password=password)
+
+            if user is not None:
+                login(request, user)
+                return redirect('home')
+            else:
+                messages.info(request, 'Username OR password is incorrect')
+
+        context = {}
+        return render(request, 'user/login.html', context)
+
 
 def register(request):
-    return render(request, "user/register.html")
+    if request.user.is_authenticated:
+        return redirect('home')
+    else:
+        form = CreateUserForm()
+        if request.method == 'POST':
+            form = CreateUserForm(request.POST)
+            if form.is_valid():
+                form.save()
+                user = form.cleaned_data.get('username')
+                messages.success(request, 'Account was created for ' + user)
+
+                return redirect('login')
+
+        context = {'form': form}
+        return render(request, 'user/register.html', context)
+
+
+def logOut(request):
+	logout(request)
+	return redirect('login')
+
