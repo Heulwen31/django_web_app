d890e9d8f6f20e739c81f6746026777fdcd41a3e
Rukitori <rukitori1620@gmail.com>
2021-04-01 18:07:39 +0700

    product_id complete

--- shop/urls.py
+++ shop/urls.py
@@ -3,9 +3,6 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('shop/', views.shop, name="shopPage"),
-<<<<<<< HEAD
     path('product/<int:product_id>', views.product, name="product"),
-=======
-    path('product/', views.product, name="product"),
->>>>>>> e7ad210fe9de3e17f6030a6611c6438f9c547aef
+    path('deals/', views.deals, name="deals"),
 ]
--- shop/views.py
+++ shop/views.py
@@ -5,7 +5,6 @@ from .models import Product
 # Create your views here.
 
 
-@login_required(login_url='login')
 def shop(request):
     queryset = Product.objects.all()
     context = {
@@ -13,15 +12,15 @@ def shop(request):
     }
     return render(request, 'shop/index.html', context)
 
-<<<<<<< HEAD
 def product(request, product_id):
     product = Product.objects.get(id=product_id)
     return render(request, 'shop/product.html', {'product': product})
-=======
-def product(request):
+
+@login_required(login_url='login')
+def deals(request):
     queryset = Product.objects.all()
     context = {
         "object_list": queryset
     }
-    return render(request, 'shop/product.html', context)
->>>>>>> e7ad210fe9de3e17f6030a6611c6438f9c547aef
+    return render(request, 'shop/index.html', context)
+
